7dfc456816a887062cacd1e9f53f11feecb0e77e
walid horchani <walid.horchani@oyez.fr>
2020-07-10 14:25:59 +0100

    styling startGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,15 +7,27 @@ import IndicatorContainer from '../components/IndicatorContainer';
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <IndicatorContainer style={styles.indicator}>
-        <Text>T</Text>
-      </IndicatorContainer>
-      <IndicatorContainer style={styles.indicator}>
-        <Text>V</Text>
-      </IndicatorContainer>
-      <IndicatorContainer style={styles.indicator}>
-        <Text>X</Text>
-      </IndicatorContainer>
+      <View style={styles.descriptionContainer}>
+        <IndicatorContainer style={styles.indicator}>
+          <Text>T</Text>
+        </IndicatorContainer>
+        <Text style={styles.text}>Numero dans sa place exacte</Text>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <IndicatorContainer style={styles.indicator}>
+          <Text>V</Text>
+        </IndicatorContainer>
+
+        <Text style={styles.text}>
+          Numero existe mais pas dans sa place exacte
+        </Text>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <IndicatorContainer style={styles.indicator}>
+          <Text>X</Text>
+        </IndicatorContainer>
+        <Text style={styles.text}>Numero n'existe pas</Text>
+      </View>
       <View style={styles.buttonContainer}>
         <Button title="Start" color={colors.secondo} />
       </View>
@@ -26,6 +38,8 @@ const StartGameScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     alignItems: 'center',
+    width: '100%',
+    padding: 10,
   },
   buttonContainer: {
     width: 100,
@@ -34,6 +48,16 @@ const styles = StyleSheet.create({
   indicator: {
     width: 40,
     height: 40,
+  },
+  descriptionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  text: {
+    flex: 1,
+    padding: 10,
   },
 });
 
