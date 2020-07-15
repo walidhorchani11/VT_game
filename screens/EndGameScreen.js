a79ae3bd9c786d0632a2b242d6ead45ea00477fd
walid horchani <walid.horchani@oyez.fr>
2020-07-15 16:19:56 +0100

    add image to gameOver screen

--- screens/EndGameScreen.js
+++ screens/EndGameScreen.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import defaults_styles from '../constants/defaults_styles';
+import colors from '../constants/colors';
 
 const EndGameSceen = (props) => {
   return (
-    <View>
+    <View style={defaults_styles.screen}>
       <Text>game over....::: {props.countTry}</Text>
-      <Text>number issss....::: {props.rdmNumber}</Text>
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/success.gif')} style={styles.image} />
+      </View>
+      <Text>number: {props.rdmNumber}</Text>
+      <Text>nombre d'essaie: {props.rdmNumber}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 270,
+    height: 270,
+    backgroundColor: 'grey',
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: colors.secondo,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default EndGameSceen;
